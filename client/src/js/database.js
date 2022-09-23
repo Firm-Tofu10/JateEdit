e7cf2f5c7f86e83827e5df6429e5a1d2cc0d8bd5
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-22 18:34:53 -0600

    Trying to implement PutDB&GetAllDB
    Brian pointed me to PWA day3 for reference

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+console.log('PUT to the database');
+const jateDB = await openDB("jate", 1);
+const transition = await openDB("jate", "readwrite");
+const store = transition.objectStore("jate");
+const request = store.put({id: id, jate: content });
+const result = await request;
+console.log("Saved to Data", result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getAllDb = async () => {
+	console.log('GET all from the database');
+	const jateDB = await openDB("jate", 1);
+	const transition = jateDB.transaction("jate","readonly");
+	const store = jateDB.objectStore("jate");
+	const request = store.getALL();  
+	// get(1); might be correct but the comment says get all?
+	const result = await request;
+	// result ("Thought this may work like return at the end of a if?")
+	console.log("Got data from DB",result)
+	console.log("Did not get from DB");
+	return result;
+};
+
 
 initdb();
